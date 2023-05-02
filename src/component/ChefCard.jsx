43c16d4b6af814988fadb79c1bf204a942e699c6
ashraf56 <ashrafulfahim07@gmail.com>
2023-05-02 15:23:19 +0600

    set all card into home route

--- src/component/ChefCard.jsx
+++ src/component/ChefCard.jsx
@@ -3,18 +3,28 @@ import { CardGroup } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row'
+import {  FaRegThumbsUp } from "react-icons/fa";
+
 
 const ChefCard = ({singlechef}) => {
-    let {id ,likes,description,chef_name,years_of_experience}=singlechef;
+    let {id ,likes,chef_picture,chef_name,years_of_experience,number_of_recipes}=singlechef;
     return (
         <div>
           
-  <div className="col">
-    <div className="card">
-      <img src="..." className="card-img-top" />
-      <div className="card-body">
+  <div className="col ">
+    <div className="card rounded-5">
+      <div className="card-body m-2 ">
+      <div className='py-3'>
+        <img src={chef_picture} className="img-fluid w-25 rounded-5" />
+      </div>
+      
+      
         <h5 className="card-title">{chef_name}</h5>
-        <p className="card-text">{years_of_experience}</p>
+        <p className=" fs-semibold">Experience:{years_of_experience} years</p>
+        <p className=" fs-semibold"><FaRegThumbsUp/>  {likes} </p>
+        <p className=" fs-semibold">number of recipes :{number_of_recipes} </p>
+        <button className='btn btn-outline-danger'>View Recipes</button>
+      
       </div>
     </div>
   </div>
--- src/component/Chefs.jsx
+++ src/component/Chefs.jsx
@@ -11,8 +11,9 @@ fetch('https://b7a10-chef-recipe-hunter-server-side-ashraf56.vercel.app/chef')
 },[])
 
     return (
-        <div>
-          <h1>{chef.length}</h1>
+        <div className='m-5  my-5'>
+          <h1 className='fw-bold py-4 text-center'>Meet Our Talented  <span className='text-danger'>Bangladeshi Chefs</span>
+</h1>
 
 
           <div className="row row-cols-1 row-cols-md-2 g-4">
