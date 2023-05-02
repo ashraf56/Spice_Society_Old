8ed040d50597123ad22f9924babea98c4e10a4c5
ashraf56 <ashrafulfahim07@gmail.com>
2023-05-02 11:59:26 +0600

    about section completd

--- src/component/ChefCard.jsx
+++ src/component/ChefCard.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
+import { CardGroup } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row'
-const ChefCard = () => {
+
+const ChefCard = ({singlechef}) => {
+    let {id ,likes,description,chef_name,years_of_experience}=singlechef;
     return (
         <div>
-             <Row xs={1} md={2} className="g-4">
-    
-    <Col>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit
-            longer.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
+          
+  <div className="col">
+    <div className="card">
+      <img src="..." className="card-img-top" />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  
 
-</Row>
 
 
         </div>
--- src/component/Chefs.jsx
+++ src/component/Chefs.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ChefCard from './ChefCard';
 ;
 const Chefs = () => {
 let [chef,setChef]=useState([]);
@@ -13,6 +14,18 @@ fetch('https://b7a10-chef-recipe-hunter-server-side-ashraf56.vercel.app/chef')
         <div>
           <h1>{chef.length}</h1>
 
+
+          <div className="row row-cols-1 row-cols-md-2 g-4">
+
+{
+    chef.map(singlechef=> <ChefCard
+    key={singlechef.id}
+    singlechef={singlechef}
+    
+    ></ChefCard>)
+}
+
+          </div>
 
    
 
